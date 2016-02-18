fed33d8bcc94212b71b7b949a80150fccf7875f1
Anton Ulyanov <rumburu@gmail.com>
2016-02-18 13:10:48 +0300

    added avatars

---
+++ Example/Pods/Target Support Files/Pods-BUMPMessanger-Theme-BUMPMessanger-Theme/Pods-BUMPMessanger-Theme-BUMPMessanger-Theme-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BUMPMessanger_Theme_BUMPMessanger_Theme : NSObject
+@end
+@implementation PodsDummy_Pods_BUMPMessanger_Theme_BUMPMessanger_Theme
+@end
---
+++ Example/Pods/Target Support Files/Pods-BUMPMessanger-Theme/Pods-BUMPMessanger-Theme-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// BUMPMessanger-Theme
+#define COCOAPODS_POD_AVAILABLE_BUMPMessanger_Theme
+#define COCOAPODS_VERSION_MAJOR_BUMPMessanger_Theme 0
+#define COCOAPODS_VERSION_MINOR_BUMPMessanger_Theme 1
+#define COCOAPODS_VERSION_PATCH_BUMPMessanger_Theme 0
+
